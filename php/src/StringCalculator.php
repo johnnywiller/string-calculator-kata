091da13cb082e1e80e3c3d25a1e9ecac8b247c84
Sergey Kudashev <kudashevs@gmail.com>
2024-02-13 12:19:13 +0100

    Add PHP version

---
+++ php/src/StringCalculator.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StringCalculatorKata;
+
+class StringCalculator
+{
+    /**
+     * Calculate a sum from a string of numbers.
+     *
+     * @param string $numbers
+     * @return int
+     */
+    public function add(string $numbers): int
+    {
+        return 0;
+    }
+}
---
+++ php/tests/StringCalculatorTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StringCalculatorKata\Tests;
+
+use PHPUnit\Framework\TestCase;
+use StringCalculatorKata\StringCalculator;
+
+class StringCalculatorTest extends TestCase
+{
+    /** @test */
+    public function it_can_pass_an_example_test()
+    {
+        $this->assertTrue(true);
+    }
+}
