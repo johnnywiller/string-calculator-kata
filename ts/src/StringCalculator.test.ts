6a0464413c61ce3d8826577826f01cee4ff10ac6
Sergey Kudashev <kudashevs@gmail.com>
2024-02-15 20:36:15 +0300

    Add TypeScript version

---
+++ ts/src/StringCalculator.test.ts
@@ -0,0 +1,5 @@
+describe('StringCalculator test suite', () => {
+  it('can pass an example test', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ ts/src/StringCalculator.ts
@@ -0,0 +1,11 @@
+export class StringCalculator {
+    /**
+     * Calculate a sum from a string of numbers.
+     *
+     * @param {string} numbers
+     * @return {number}
+     */
+    public add(numbers: string): number {
+        return 0;
+    }
+}
